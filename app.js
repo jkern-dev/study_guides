fb3dac2f187b1e495f46c39005e0c1a1c1fc2bc5
Josh Kern <jkern.dev@gmail.com>
2019-08-15 13:59:56 -0700

    test initial api routes

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const guides = require("./routes/api/guides");
+
+
+// mongoDB connection
+mongoose
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log("connected to mongoDB"))
+  .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+});
+
+app.use("/api/users", users);
+app.use("/api/guides", guides);
+
 const port = process.env.PORT || 5000;
-
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-app.get("/", (req,res) => res.send("Hello World!"));
+
 
