fff0b9ddc830c9e00d3dc03df61aa3ba6e23e8b6
Josh Kern <jkern.dev@gmail.com>
2019-08-17 11:23:58 -0700

    journal reducer to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
+import journals from './journal_reducer';
 
 const RootReducer = combineReducers({
   session,
+  journals,
   errors
 });
 
